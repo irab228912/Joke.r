3ade095fdfe4abc52510e0ab8cee236b9f978796
irab228912 <irab65554@gmail.com>
2023-03-25 19:10:50 +0200

    site finished

--- index.js
+++ index.js
@@ -1,91 +1,102 @@
 const http = require('http');
-const url = require('url');
 const path = require('path');
+const url = require('url');
 const fs = require('fs');
+const dataPath = 'data';
 
-let html = ''
-
-let server = http.createServer(function (req, res) {
-	if (req.url == '/jokes' && req.method == 'GET') {
-		getAllJokes(req, res);
-	}
-	else if (req.url == '/jokes' && req.method == 'POST') {
-		addJoke(req, res);
-	}
-	else if (req.url.startsWith('/like') && req.method == 'GET') {
-		like(req, res);
-	}
-	else if (req.url.startsWith('/dislike') && req.method == 'GET') {
-		// .startsWith(`/like`)
-		dislike(req, res);
-	}
-	else {
-		res.writeHead(404, {'Content-Type':'text/html',});
-		html = "<h3>Error 404!!!</h3>";
-		res.end(html);
-	}
+const server = http.createServer((req, res)=> {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        if(req.url == '/jokes' && req.method == 'GET') {
+            return getAllJokes(req, res);
+        }
+        if(req.url == '/jokes' && req.method == 'POST') {
+            return addJoke(req, res);
+        }
+        if(req.url.startsWith('/like')) {
+            return like(req, res);
+        }
+        if(req.url.startsWith('/dislike')) {
+            return dislike(req, res);
+        }
+        res.statusCode = 404;
+        return res.end('Error 404');
 });
-
 server.listen(3000);
 
 function getAllJokes(req, res) {
-	let dir = fs.readdirSync('data');
-	let allJokes = [];
-	for (var i = 0; i < dir.length; i++) {
-		let file = fs.readFileSync(path.join('data', i+'.json'));
-		let jokeJson = file.toString();
-	    let joke = JSON.parse(jokeJson);
-	    joke.id = i;
+    let dir = fs.readdirSync(dataPath);
+    let allJokes = [];
+    for(let i = 0; i < dir.length; i++) {
+        let file = fs.readFileSync(path.join(dataPath, i+'.json'));
+        let jokeJson = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJson);
+        joke.id = i;
 
-	allJokes.push(joke);
-	}
-	res.writeHead(200, {
-		'Content-Type':'application/json',
-		'charset':'utf-8'
-	});
-	res.end(JSON.stringify(allJokes));	
+        allJokes.push(joke);
+    }
+    return res.end(JSON.stringify(allJokes));
 }
 
-function addJoke(req, res){
-	let data = '';
-		req .on('data', function(chunk){
-			data += chunk;
-		})
-		req.on('end', function(){
-			let joke = JSON.parse(data);
-			joke.likes = 0;
-			joke.dislikes = 0;
+function addJoke(req, res) {
+    let data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        let joke = JSON.parse(data);
+        joke.likes = 0;
+        joke.dislikes = 0;
 
-			let dir = fs.readdirSync('data');
-			let fileName = dir.length+'json';
-			fs.writeFileSync(path.join('data', fileName),JSON.stringify(joke));
+        let dir = fs.readdirSync(dataPath);
+        let fileName = dir.length+'.json';
+        let filePath = path.join(dataPath, fileName);
 
-			res.end();
-		})
+        
+        fs.writeFileSync(filePath, JSON.stringify(joke));
+        res.statusCode = '201';
+        return res.end(JSON.stringify(joke));
+    });
 }
 
-function like(req, res){
-	let params = url.parse(req.url, true).query;
-	let id = params.id;
-	let dir = fs.readdirSync('data');
-	let fileName = path.join('data', id+'.json');
-	let file = fs.readFileSync(fileName);
-	let jokeJson = file.toString();
-	let joke = JSON.parse(jokeJson);
-	joke.likes++;
-	fs.writeFileSync(fileName, JSON.stringify(joke));
-	res.end();
+function like(req, res) {
+    const url = require('url');
+    const params = url.parse(req.url, true).query;
+    let id = params.id;
+    if(id) {
+        let filePath = path.join(dataPath, id+'.json') 
+        let file = fs.readFileSync(filePath);
+        let jokeJSON = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJSON);
+    
+        joke.likes++;
+    
+        fs.writeFileSync(filePath, JSON.stringify(joke));
+
+        joke.id = id;
+
+        return res.end(JSON.stringify(joke));
+    }
+    res.statusCode = '400';
+    return res.end('Bad request');
 }
 
-function dislike(req, res){
-	let params = url.parse(req.url, true).query;
-	let id = params.id;
-	let dir = fs.readdirSync('data');
-	let fileName = path.join('data', id+'.json');
-	let file = fs.readFileSync(fileName);
-	let jokeJson = file.toString();
-	let joke = JSON.parse(jokeJson);
-	joke.dislikes++;
-	fs.writeFileSync(fileName, JSON.stringify(joke));
-	res.end();
+function dislike(req, res) {
+    const params = url.parse(req.url, true).query;
+    let id = params.id;
+    if(id){
+        let filePath = path.join(dataPath, id+'.json')
+        let file = fs.readFileSync(filePath);
+        let jokeJSON = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJSON);
+    
+        joke.dislikes++;
+    
+        fs.writeFileSync(filePath, JSON.stringify(joke));
+
+        joke.id = id;
+
+        return res.end(JSON.stringify(joke));
+    }
+    res.statusCode = '400';
+    return res.end('Bad request');
 }
